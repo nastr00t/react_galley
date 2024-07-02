78a29d867f40101a2db12e23662e5e901342c3c6
nastr00t <elvis.acosta@gmail.com>
2024-07-02 06:43:59 -0500

    Comet route por spa

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navigation from './components/Navigation'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Photo1 } from './components/Photo1'
 import { Photo2 } from './components/Photo2'
 import { Photo3 } from './components/Photo3'
@@ -7,22 +7,22 @@ import { Photo4 } from './components/Photo4'
 import { Photo5 } from './components/Photo5'
 import { Photo6 } from './components/Photo6' 
 
-
 function App() {
 
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/photo1' element={<Photo1 />} />
+          <Route path='/photo2' element={<Photo2 />}  />
+          <Route path='/photo3' element={<Photo3 />} />
+          <Route path='/photo4' element={<Photo4 />} />
+          <Route path='/photo5' element={<Photo5 />} />
+          <Route path='/photo6' element={<Photo6 />} />
+        </Routes>
+      </BrowserRouter>
 
-      <div className='container'>
-        <h1 id="title" className='text-center my-3'>Galería</h1>
-      </div>
-      <Photo1 />
-      <Photo2 />
-      <Photo3 />
-      <Photo4 />
-      <Photo5 />
-      <Photo6 />
     </>
   )
 }
